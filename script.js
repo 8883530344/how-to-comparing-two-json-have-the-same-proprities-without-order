df7a1bdcedfee6c3c1da33173ff14454c574175f
8883530344 <109344624+8883530344@users.noreply.github.com>
2022-09-16 14:12:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,5 @@ request.send();
 request.onload=function(){
     var result=JSON.parse(request.response);
     for (var i=0; i<result.length; i++)
-    console.log(result[i].name,result[i].flags);
+    console.log(result[i].name,result[i].region,result[i].subregion,result[i].population);
 };
-
